b9bea6220441eb141d5862b3c278b5b5bacdc61b
olgakonyukhova <olga.konyukhova@realize.dev>
2025-01-27 23:20:32 +0300

    добавлены вспомогательные методы

--- new_test.py
+++ new_test.py
@@ -13,32 +13,51 @@ class TestCatalog:
         self.driver.quit()
 
     def test_catalog(self):
-        # Открываем сайт
-        self.driver.get("https://sokolov.ru/")
+        self.open_site()
 
         # Локаторы
         catalog_button_locator = (By.XPATH, '//button[@data-qa="header_catalog_nav_open_btn"]')
         all_categories_locator = (By.XPATH, '//a[@data-qa="all_of_this_category"]')
         product_tiles_locator = (By.XPATH, '//a[@data-qa-article]')  # Локатор для всех плиток товаров
 
-        # Ждем, пока загрузится кнопка "Каталог", и нажимаем
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(catalog_button_locator)
-        ).click()
+        self.open_catalog(catalog_button_locator)
 
-        # Ждем и переходим во "Все категории"
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(all_categories_locator)
-        ).click()
+        self.open_all_categories(all_categories_locator)
 
+        self.wait_for_product_tiles(product_tiles_locator)
+
+        product_tiles = self.count_and_log_product_tiles(product_tiles_locator)
+
+        self.assert_tile_count_is_72(product_tiles)
+
+    def assert_tile_count_is_72(self, product_tiles):
+        # Проверка, что плиток ровно 72
+        assert len(product_tiles) == 72, f"Ожидалось 72 товара, но найдено {len(product_tiles)}"
+
+    def count_and_log_product_tiles(self, product_tiles_locator):
+        # Проверяем количество найденных плиток
+        product_tiles = self.driver.find_elements(*product_tiles_locator)
+        print(f"Найдено плиток: {len(product_tiles)}")  # Выводим количество плиток в консоль
+        return product_tiles
+
+    def wait_for_product_tiles(self, product_tiles_locator):
         # Ожидаем, пока появится хотя бы одна плитка товара
         WebDriverWait(self.driver, 20).until(
             EC.presence_of_all_elements_located(product_tiles_locator)
         )
 
-        # Проверяем количество найденных плиток
-        product_tiles = self.driver.find_elements(*product_tiles_locator)
-        print(f"Найдено плиток: {len(product_tiles)}")  # Выводим количество плиток в консоль
+    def open_all_categories(self, all_categories_locator):
+        # Ждем и переходим во "Все категории"
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(all_categories_locator)
+        ).click()
 
-        # Проверка, что плиток ровно 72
-        assert len(product_tiles) == 72, f"Ожидалось 72 товара, но найдено {len(product_tiles)}"
+    def open_catalog(self, catalog_button_locator):
+        # Ждем, пока загрузится кнопка "Каталог", и нажимаем
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(catalog_button_locator)
+        ).click()
+
+    def open_site(self):
+        # Открываем сайт
+        self.driver.get("https://sokolov.ru/")
