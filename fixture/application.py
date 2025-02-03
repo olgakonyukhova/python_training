06b6550c8545ea212894e559610199a97a2edd07
olgakonyukhova <olga.konyukhova@realize.dev>
2025-02-04 00:05:22 +0300

    Update application.py

--- fixture/application.py
+++ fixture/application.py
@@ -1,8 +1,13 @@
+from fixture.driver_helper import DriverHelper
+
+
 class Application:
     def __init__(self):
         from selenium import webdriver
         self.driver = webdriver.Chrome()
         self.driver.set_window_size(1512, 859)
+
+        self.driver_helper = DriverHelper(self.driver)
 
     def destroy(self):
         self.driver.quit()
@@ -11,11 +16,8 @@ class Application:
         self.driver.get(url)
 
     def wait_for_elements(self, locator, timeout=20):
-        WebDriverWait(self.driver, timeout).until(
-            EC.presence_of_all_elements_located(locator)
-        )
+        self.driver_helper.wait_element_visible(locator, timeout)
 
     def wait_and_click(self, locator, timeout=10):
-        WebDriverWait(self.driver, timeout).until(
-            EC.element_to_be_clickable(locator)
-        ).click()
+        self.driver_helper.wait_for_clickable(locator, timeout)
+        self.driver_helper.click(locator)
