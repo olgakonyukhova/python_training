9a9a53ae1f6767b47ec221fa7a8c7d8f219f54a0
olgakonyukhova <olga.konyukhova@realize.dev>
2025-02-04 00:02:35 +0300

    Обновленный CatalogPage, используя DriverHelper

--- fixture/CatalogPage.py
+++ fixture/CatalogPage.py
@@ -1,37 +1,25 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from model.locators import CatalogPageLocators  # Импортируем класс локаторов
+from model.locators import CatalogPageLocators
+from .driver_helper import DriverHelper
 
 class CatalogPage:
     def __init__(self, driver):
-        self.driver = driver  # Сохраняем драйвер
+        self.driver = driver
+        self.helper = DriverHelper(driver)  # Создаем объект DriverHelper
 
     def open_catalog(self):
         """Открытие каталога"""
-        self.wait_and_click(CatalogPageLocators.CATALOG_BUTTON)
+        self.helper.click(CatalogPageLocators.CATALOG_BUTTON)
 
     def open_all_categories(self):
         """Открытие всех категорий"""
-        self.wait_and_click(CatalogPageLocators.ALL_CATEGORIES)
+        self.helper.click(CatalogPageLocators.ALL_CATEGORIES)
 
     def wait_for_product_tiles(self):
         """Ожидание загрузки карточек товаров"""
-        self.wait_for_elements(CatalogPageLocators.PRODUCT_TILES)
+        self.helper.wait_element_visible(CatalogPageLocators.PRODUCT_TILES)
 
     def count_and_log_product_tiles(self):
         """Подсчет и логирование количества карточек товаров"""
         product_tiles = self.driver.find_elements(*CatalogPageLocators.PRODUCT_TILES)
         print(f"Найдено плиток: {len(product_tiles)}")
         return product_tiles
-
-    def wait_for_elements(self, locator, timeout=20):
-        """Ожидание появления элементов"""
-        WebDriverWait(self.driver, timeout).until(
-            EC.presence_of_all_elements_located(locator)
-        )
-
-    def wait_and_click(self, locator, timeout=10):
-        """Ожидание кликабельности элемента и клик"""
-        WebDriverWait(self.driver, timeout).until(
-            EC.element_to_be_clickable(locator)
-        ).click()
