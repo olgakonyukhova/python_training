2f104b84fc93bee3e315f2450c7eaabaed9234de
olgakonyukhova <olga.konyukhova@realize.dev>
2025-02-03 23:34:22 +0300

    преобразовала локаторы в класс

--- model/locators.py
+++ model/locators.py
@@ -1,10 +1,7 @@
 from selenium.webdriver.common.by import By
 
-def get_catalog_button_locator():
-    return (By.XPATH, '//button[@data-qa="header_catalog_nav_open_btn"]')
-
-def get_all_categories_locator():
-    return (By.XPATH, '//a[@data-qa="all_of_this_category"]')
-
-def get_product_tiles_locator():
-    return (By.XPATH, '//a[@data-qa-article]')
+class CatalogPageLocators:
+    """Локаторы для страницы каталога"""
+    CATALOG_BUTTON = (By.XPATH, '//button[@data-qa="header_catalog_nav_open_btn"]')
+    ALL_CATEGORIES = (By.XPATH, '//a[@data-qa="all_of_this_category"]')
+    PRODUCT_TILES = (By.XPATH, '//a[@data-qa-article]')
